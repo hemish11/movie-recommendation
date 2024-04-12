b1dca53f7053ffaad39086be072805d300ad08af
Hemish Pancholi <hemishpancholi11@gmail.com>
2024-04-12 23:13:52 +0530

    Added support for posters

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MovieSearchScreen extends StatefulWidget {
   const MovieSearchScreen({super.key});
 
   @override
-  _MovieSearchScreenState createState() => _MovieSearchScreenState();
+  State<MovieSearchScreen> createState() => _MovieSearchScreenState();
 }
 
 class _MovieSearchScreenState extends State<MovieSearchScreen> {
@@ -43,90 +43,150 @@ class _MovieSearchScreenState extends State<MovieSearchScreen> {
 
     if (response.statusCode == 200) {
       final List<dynamic> data = jsonDecode(response.body);
-      setState(() {
-        _searchResults = data.map((json) => Movie.fromJson(json)).toList();
-      });
+
+      _searchResults = data.map((json) => Movie.fromJson(json)).toList();
     } else {
       throw Exception('Failed to load movies');
+    }
+
+    await _fetchPostersForMovies();
+
+    setState(() {});
+  }
+
+  Future<void> _fetchPostersForMovies() async {
+    for (Movie movie in _searchResults) {
+      final response = await http.get(
+        Uri.parse('https://api.themoviedb.org/3/movie/${movie.id}/images'),
+        headers: {
+          'Authorization':
+              'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YTk2ZDMzZmJhODk2NDA2NDQzZWY2MDg1ZmU3OTI4YyIsInN1YiI6IjY2MTdjMTBiZDhmNDRlMDE3YzJlZWJiZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.C-KDq4zVev8-urTF_O261wgjVER2wIVhpaV8z77uR8g'
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = jsonDecode(response.body);
+        movie.posterUrl =
+            'https://image.tmdb.org/t/p/original${data['posters'].firstWhere(
+          (poster) => poster['iso_639_1'] == 'en',
+          orElse: () => null,
+        )['file_path']}';
+      }
     }
   }
 
   void _showMovieDetails(BuildContext context, Movie movie) {
     showModalBottomSheet(
       context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
       builder: (BuildContext context) {
         return Container(
-          padding: const EdgeInsets.all(20),
+          height: MediaQuery.of(context).size.height * 0.75,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(25.0),
+              topRight: Radius.circular(25.0),
+            ),
+          ),
           child: SingleChildScrollView(
+            physics: const ClampingScrollPhysics(),
             child: SafeArea(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text(
-                    movie.title,
-                    style: const TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(24),
+                    child: Image.network(movie.posterUrl),
                   ),
-                  const SizedBox(height: 8),
-                  const Text(
-                    'Overview:',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(movie.overview),
-                  const SizedBox(height: 8),
-                  const Text(
-                    'Genres:',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Wrap(
-                    spacing: 8,
-                    runSpacing: 12,
-                    children: List.generate(
-                      movie.genre.length,
-                      (index) => Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 12,
-                          vertical: 4,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.grey.withOpacity(0.4),
-                          borderRadius: BorderRadius.circular(100),
-                        ),
-                        child: Text(movie.genre[index]),
+                  const SizedBox(height: 32),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      movie.title,
+                      style: const TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
                   const SizedBox(height: 8),
-                  const Text(
-                    'Cast:',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      'Overview:',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                  Wrap(
-                    spacing: 8,
-                    runSpacing: 12,
-                    children: List.generate(
-                      movie.cast.length,
-                      (index) => Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 12,
-                          vertical: 4,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(movie.overview),
+                  ),
+                  const SizedBox(height: 8),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      'Genres:',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Wrap(
+                      spacing: 8,
+                      runSpacing: 12,
+                      children: List.generate(
+                        movie.genre.length,
+                        (index) => Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 12,
+                            vertical: 4,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.withOpacity(0.4),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Text(movie.genre[index]),
                         ),
-                        decoration: BoxDecoration(
-                          color: Colors.grey.withOpacity(0.4),
-                          borderRadius: BorderRadius.circular(100),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Text(
+                      'Cast:',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Wrap(
+                      spacing: 8,
+                      runSpacing: 12,
+                      children: List.generate(
+                        movie.cast.length,
+                        (index) => Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 12,
+                            vertical: 4,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.withOpacity(0.4),
+                            borderRadius: BorderRadius.circular(100),
+                          ),
+                          child: Text(movie.cast[index]),
                         ),
-                        child: Text(movie.cast[index]),
                       ),
                     ),
                   ),
@@ -150,8 +210,8 @@ class _MovieSearchScreenState extends State<MovieSearchScreen> {
             hintStyle: const TextStyle(color: Colors.white),
             suffixIcon: IconButton(
               icon: const Icon(Icons.search, color: Colors.white),
-              onPressed: () {
-                _searchMovies(_searchController.text);
+              onPressed: () async {
+                await _searchMovies(_searchController.text);
               },
             ),
             border: InputBorder.none,
@@ -169,20 +229,42 @@ class _MovieSearchScreenState extends State<MovieSearchScreen> {
                     _showMovieDetails(context, _searchResults[index]);
                   },
                   child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                     child: Card(
                       elevation: 4,
-                      child: ListTile(
-                        title: Text(
-                          _searchResults[index].title,
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 18),
-                        ),
-                        subtitle: Text(
-                          _searchResults[index].overview,
-                          maxLines: 3,
-                          overflow: TextOverflow.ellipsis,
-                        ),
+                      margin: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(16),
+                            child: Image.network(
+                              _searchResults[index].posterUrl,
+                            ),
+                          ),
+                          const SizedBox(height: 28),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            child: Text(
+                              _searchResults[index].title,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 12),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            child: Text(
+                              _searchResults[index].overview,
+                              maxLines: 3,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                          const SizedBox(height: 16),
+                        ],
                       ),
                     ),
                   ),
@@ -205,6 +287,7 @@ class Movie {
   final List<String> genre;
   final String overview;
   final List<String> cast;
+  String posterUrl; // Add this property
 
   Movie({
     required this.id,
@@ -212,6 +295,7 @@ class Movie {
     required this.genre,
     required this.overview,
     required this.cast,
+    required this.posterUrl,
   });
 
   factory Movie.fromJson(Map<String, dynamic> json) {
@@ -221,6 +305,7 @@ class Movie {
       genre: List<String>.from(json['genre']),
       overview: json['overview'],
       cast: List<String>.from(json['cast']),
+      posterUrl: '', // Initialize with empty string
     );
   }
 
@@ -231,6 +316,7 @@ class Movie {
       'genre': genre,
       'overview': overview,
       'cast': cast,
+      'posterUrl': posterUrl,
     };
   }
 }
